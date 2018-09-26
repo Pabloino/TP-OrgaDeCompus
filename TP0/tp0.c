308abf4c0ba8cf90feb51dffe20250bd72ec939b
mvbattan <mvbattan@gmail.com>
2018-09-26 03:07:19 -0300

    Adding I/O validations

--- TP0/tp0.c
+++ TP0/tp0.c
@@ -38,14 +38,14 @@ void base64encode(unsigned char* input, unsigned char* output) {
   output[2] = ((input[1] % 16) << 2) | (input[2] >> 6);
   output[3] = input[2] % 64;
 
-  for (int i = 0; i < 4 ; i++){
+  for (int i = 0; i < 4 ; i++) {
     output[i] = base64chars[(uint8_t) output[i]];
   }
 
 }
 void base64decode(unsigned char* encoded_input, unsigned char* output) {
 
-  for (int i = 0; i < 4 ; i++){
+  for (int i = 0; i < 4 ; i++) {
     encoded_input[i] = decode_ch(encoded_input[i]);
   }
 
@@ -54,22 +54,36 @@ void base64decode(unsigned char* encoded_input, unsigned char* output) {
   output[2] = (encoded_input[2] % 4) << 6 | (encoded_input[3] % 64);
 }
 int readB(int input, unsigned char* buf, int bytes) {
-  while (read(input, buf, 1) && --bytes) buf++;
+  while (bytes) {
+    int result = read(input, buf, 1);
+    if (result == -1) {
+      fprintf(stderr, "Error de lectura en el archivo especificado");
+      close_files(&choice);
+      exit(ERROR);
+    } else if (result == 0) break;
+    bytes -= 1;
+    buf++;
+  };
   return bytes;
 }
 
 void write_Bytes(int output, unsigned char* buf, int bytes) {
-  if (write(output, buf, bytes) != bytes) {
-    fprintf(stderr, "Error de escritura: faltaron bytes");
+  if (!bytes) return;
+
+  int result = write(output, buf, bytes);
+  if (result == -1) {
+    fprintf(stderr, "Error de escritura");
     close_files(&choice);
     exit(ERROR);
+  }
+  if (result != bytes) {
+    write_Bytes(output, buf, bytes - result);
   }
 }
 
 int padding_count(unsigned char* input, int size) {
   int count = 0;
-  for (int i = 0; i < size; i++)
-  {
+  for (int i = 0; i < size; i++) {
       count += input[i] == PADDING_CH;
   }
   return count;
@@ -83,50 +97,51 @@ void checkDecodingError(unsigned char* inputBuf) {
     exit(DECODE_ERROR);
   }
 }
+
 int main (int argc, char** argv) {
-    parseAnswer(argc, argv, &choice);
-    int bytesToFinish = 0;
-    int maxToRead = 3 + choice.decode;
-    int maxToWrite = 3 + !choice.decode;
-    bool stop;
-    int writeLenght;
+  parseAnswer(argc, argv, &choice);
+  int bytesToFinish = 0;
+  int maxToRead = 3 + choice.decode;
+  int maxToWrite = 3 + !choice.decode;
+  bool stop;
+  int writeLenght;
 
-    while (!bytesToFinish) {
-      unsigned char input[4] = {0};
-      unsigned char output[4] = {0};
+  while (!bytesToFinish) {
+    unsigned char input[4] = {0};
+    unsigned char output[4] = {0};
 
-      bytesToFinish = readB(choice.inputFile, input, maxToRead);
+    bytesToFinish = readB(choice.inputFile, input, maxToRead);
 
-      if (bytesToFinish == maxToRead) {
-        bytesToFinish = 0;
-        break;
-      }
-
-      if (bytesToFinish && choice.decode) {
-        fprintf(stderr, "Error decodificando: tamanio de codificado incorrecto.\n");
-        exit(DECODE_ERROR);
-      }
-
-      if (choice.decode) checkDecodingError(input);
-
-      writeLenght = maxToWrite - bytesToFinish - choice.decode * padding_count(input, maxToRead);
-
-      stop = input[3] == PADDING_CH;
-
-      if (!choice.decode){
-        base64encode(input, output);
-      }
-      else {
-        base64decode(input, output);
-      }
-
-      write_Bytes(choice.outputFile, output, writeLenght);
-
-      if (stop) break;
+    if (bytesToFinish == maxToRead) {
+      bytesToFinish = 0;
+      break;
     }
 
-    while (bytesToFinish--) write_Bytes(choice.outputFile, &PADDING_CH, 1);
+    if (bytesToFinish && choice.decode) {
+      fprintf(stderr, "Error decodificando: tamanio de codificado incorrecto.\n");
+      exit(DECODE_ERROR);
+    }
 
-    close_files(&choice);
-    return 0;
+    if (choice.decode) checkDecodingError(input);
+
+    writeLenght = maxToWrite - bytesToFinish - choice.decode * padding_count(input, maxToRead);
+
+    stop = input[3] == PADDING_CH;
+
+    if (!choice.decode) {
+      base64encode(input, output);
+    }
+    else {
+      base64decode(input, output);
+    }
+
+    write_Bytes(choice.outputFile, output, writeLenght);
+
+    if (stop) break;
+  }
+
+  while (bytesToFinish--) write_Bytes(choice.outputFile, &PADDING_CH, 1);
+
+  close_files(&choice);
+  return 0;
 }
